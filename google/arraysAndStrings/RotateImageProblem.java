2145ffc5ba5b8d3b88be3ce0ac9c5e77e102778d
vikkired-14 <014vikki@gmail.com>
2023-09-15 21:22:10 +0530

    Google interview questions

---
+++ google/arraysAndStrings/RotateImageProblem.java
@@ -0,0 +1,79 @@
+package google.arraysAndStrings;
+
+/*
+ * Problem URL : https://leetcode.com/problems/rotate-image
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [[7,4,1],[8,5,2],[9,6,3]]
+ * */
+public class RotateImageProblem {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        new RotateImageProblem().rotate(matrix);
+
+        System.out.println("-----");
+
+        matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        new RotateImageProblem().rotate_secondApproach(matrix);
+    }
+
+    /*
+     * Time Complexity : O(m)
+     * Space Complexity: O(1)
+     * */
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+        print(matrix);
+    }
+
+    private void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+
+    private void reflect(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+
+    private void print(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void rotate_secondApproach(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+
+        print(matrix);
+    }
+}
+
+
